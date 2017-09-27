564061394294d40f767391252660f3ae2f7fbf19
kholdcro <kevinhol@kth.se>
2017-09-27 18:08:15 +0200

    fixed bugs

--- lab2-svm/main.py
+++ lab2-svm/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import pylab, random, math
 
 # Seed random to get same results each time
-numpy.random.seed(100)
+np.random.seed(100)
 
 classA = [(random.normalvariate(-1.5,1),random.normalvariate(0.5,1),1.0)
           for i in range(5)] + \
@@ -17,19 +17,18 @@ classB = [(random.normalvariate(-0.0,0.5),random.normalvariate(-0.5,0.5),-1.0)
 
 def plot_data():
     pylab.plot([p[0] for p in classA],[p[1] for p in classA],'bo')
-    pylab.plot([p[0] for p in classB],[p[1] for p in classB],'ro')
-    
+    pylab.plot([p[0] for p in classB],[p[1] for p in classB],'ro')    
     pylab.show()
 
 def linearKernel(x, y):
 	np.transpose(x)
 	return x*y+1
 
-def polyKernel(x, y, p)
+def polyKernel(x, y, p):
 	np.transpose(x)
 	return math.pow((x*y+1),p)
 
-def radialKernel(x, y, sigma)
+def radialKernel(x, y, sigma):
 	numer = -math.pow(x-y,2)
 	denom = 2*math.pow(sigma,2)
 	return math.exp(numer/denom)
@@ -38,8 +37,8 @@ def radialKernel(x, y, sigma)
 
 def create_model(data):
     print(data.shape)
-    P = np.zeros(1,2)
-    print(P)
+    #P = np.zeros(1,2)
+    #print(P)
 
 def run():
     
